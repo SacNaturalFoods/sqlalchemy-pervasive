7643e8579ccc725a3fe2198a29e9b6d3c0c6343a
Lance Edgar <lance@edbob.org>
2013-08-13 08:57:02 -0700

    Added Fabric script with `release` command.

---
+++ fabfile.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+################################################################################
+#
+#  sqlalchemy-pervasive -- SQLAlchemy Dialect for Pervasive PSQL
+#  Copyright © 2013 Lance Edgar
+#
+#  This file is part of sqlalchemy-pervasive.
+#
+#  sqlalchemy-pervasive is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  sqlalchemy-pervasive is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  sqlalchemy-pervasive.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+
+import shutil
+from fabric.api import task, local
+
+
+@task
+def release():
+    """
+    Release a new version of 'sqlalchemy-pervasive'.
+    """
+
+    shutil.rmtree('sqlalchemy_pervasive.egg-info')
+    local('python setup.py sdist --formats=gztar register upload')
